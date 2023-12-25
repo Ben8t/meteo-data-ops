735f7385ac938266d706ac1cd35abb9db3dbac3a
Benoit Pimpaud <pimpaudben@gmail.com>
2023-12-25 23:00:12 +0100

    [feature] update flow and train

--- script/train.py
+++ script/train.py
@@ -1,18 +1,12 @@
 import pandas as pd
 import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
-
-
-data = pd.read_csv("training_data.csv").dropna()
-print(data.columns)
-
-split_size = 0.3
-target = ["temperature"]
-x = data.drop(target, axis=1)
-y = data[target]
-x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size=split_size)
-
 
 def compute_regression_metrics(y_true, y_pred):
     return {
@@ -20,12 +14,6 @@ def compute_regression_metrics(y_true, y_pred):
         "rmse": np.sqrt(mean_squared_error(y_true, y_pred)),
         "r2": r2_score(y_true, y_pred)
     }
-
-from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 
 def build_preprocessor(numeric_features: list, categorical_features: list) -> ColumnTransformer:
@@ -45,6 +33,15 @@ def build_preprocessor(numeric_features: list, categorical_features: list) -> Co
 
     return preprocessor
 
+
+data = pd.read_csv("training_data.csv").dropna()
+print(data.columns)
+
+split_size = 0.3
+target = ["temperature"]
+x = data.drop(target, axis=1)
+y = data[target]
+x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size=split_size)
 
 numeric_features, categorical_features = ["month_", "day_", "hour_", "temp_1", "temp_2", "temp_3", "temp_4"], ["station_id"]
 preprocessor = build_preprocessor(numeric_features, categorical_features)
